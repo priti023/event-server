8398ec71928ac15e753cb5bebf6e44b021ddbffe
priti@youricsoft.com <dev-team@youricsoft.com>
2020-03-14 18:36:41 +0530

    fixed port

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const eventsapi=require('./routers/eventsapi');
 
 
 const app=express();
-
 var port=process.env.PORT || 5000;
-
 app.use('/api',api);
 app.use('/eventsapi',eventsapi);
 
-http.listen(port, function() {
-    console.log('listening on : '+port);
-});
+app.listen(port);
